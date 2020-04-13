95ae2b78d240d4496554a9c437cffe71c47daaf5
Alexey Osipov <snow_man@bk.ru>
2020-04-13 15:00:04 +0400

    возвращение числа, последняя цифра которого больше (исправлено условие)

--- src/clousures6.groovy
+++ src/clousures6.groovy
@@ -2,6 +2,7 @@
 def comparison = {num1, num2 ->
     if (num1%10 > num2%10)
         return num1
-    else return num2
+    if (num1%10 < num2%10)
+        return num2
 }
-println(comparison(11, 100))
+println(comparison(11, 101))
