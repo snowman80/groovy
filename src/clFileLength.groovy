bf393991e46558a787cd8642f579f8e746ef09dc
Alexey Osipov <snow_man@bk.ru>
2020-04-13 16:33:05 +0400

    определение длины файла по имени

---
+++ src/clFileLength.groovy
@@ -0,0 +1,9 @@
+//замыкание считает длину файла в байтах
+def fileLength = {name ->
+    file = new File(name)
+    if(!file.exists()) {
+        println "file not found"
+    }
+     else println(file.length())}
+//передаем в замыкание имя файла
+fileLength("D:\\EPAM\\Xpath\\sample.xml")
