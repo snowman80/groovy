34148b44aab730ac8e2889467cba6b87747c09c1
Alexey Osipov <snow_man@bk.ru>
2020-04-14 17:01:08 +0400

    изменен метод сортировки массива

--- src/clousures.groovy
+++ src/clousures.groovy
@@ -1,6 +1,34 @@
 //создаем массив и заполняем значениями
 def arr = [2, 3, 8, 500, -5, 0 ,101]
 //описываем замыкание, которое сортирует по возрастанию
-def sortirovka = {a -> a.sort()}
+def mergeSort(list) {
+    mergeSort(list, 0, list.size() - 1) }
+
+def mergeSort(list, start, end) {
+    if (start < end) {
+        def middle = (int) ((start + end) / 2)
+    mergeSort(list, start, middle)
+        mergeSort(list, middle + 1, end)
+        doMerge(list, start, middle, end)  }
+    return list }
+
+def doMerge(list, start, middle, end) {
+    def l = list[start..middle]
+    def r = list[middle + 1..end]
+    l += 10000000
+    r += 10000000
+    def i = 0
+    def j = 0
+    for (k in start..end) {
+        if (l[i] <= r[j]) {
+            list[k] = l[i]
+            i += 1    }
+        else {
+            list[k] = r[j]
+            j += 1
+        }
+    }
+}
+
 //вызываем замыкание
-print sortirovka(arr)
+print mergeSort(arr)
