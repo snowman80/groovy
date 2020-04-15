4bba7ac82327b68bdea19d92b7d35a0e243da7e4
Alexey Osipov <snow_man@bk.ru>
2020-04-15 14:18:09 +0400

    выводятся все подпапки

--- src/dirList.groovy
+++ src/dirList.groovy
@@ -1,4 +1,7 @@
+import groovy.io.FileType
+
 //выводит подпапки  в папке
-def path = 'C:\\Windows\\'
-dir = new File(path)
-dir.eachDir{println it}
+def path = 'D:\\EPAM\\'
+def dir = new File(path)
+
+dir.eachFileRecurse(FileType.DIRECTORIES){println it}
