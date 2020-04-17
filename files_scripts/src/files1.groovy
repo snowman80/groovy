87eb1b29848080f9f8bfa84626e119ec95021db3
Alexey Osipov <snow_man@bk.ru>
2020-04-17 11:53:16 +0400

    исправлена логика генератора чисел цикл заменен на замыкание с range последняя строка не пустая создание файла - исправлено

--- files_scripts/src/files1.groovy
+++ files_scripts/src/files1.groovy
@@ -1,4 +1,4 @@
-//Создает 10 файлов из массива names с расшиирением txt, заполняет их случайными целыми числами
+//Создает 10 файлов из массива names с расширением txt, заполняет их случайными целыми числами
 //от 1 до 200 в каждой строке
 
 //папка, куда будут сохранены файлы
@@ -6,15 +6,17 @@ def path = "D:\\EPAM\\groovy_projects\\files_scripts\\"
 //генератор чисел и запись их в файл по одному в каждой строке
 def fileWriter = {filename ->
         def sb = new StringBuilder()
-        for (i=0; i<100000; i++) {
-        sb << Math.round(Math.random()*200)
+        (1..99999).each {
+        sb << (int)(Math.random()*200)+1
         sb  << "\n"
         }
+        //дописываем  последнюю строку без перевода каретки
+        sb << (int)(Math.random()*200)+1
         filename << sb.toString()
  }
 //создаем файлы и сразу их заполняем
-def fileCreator = {name -> fileWriter(new File("$path$name"+".txt"))}
+def fileCreator = {name -> fileWriter(new File("$path", "$name"))}
 //имена файлов
-def names = ["file1", "file2", "file3", "file4", "file5", "file6", "file7", "file8", "file9", "file10"]
+def names = ["file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt", "file6.txt", "file7.txt", "file8.txt", "file9.txt", "file10.txt"]
 
 names.each {fileCreator(it)}
