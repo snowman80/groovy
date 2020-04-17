52c9fbc8a57200b86e398e9fdee4528a0676b392
Alexey Osipov <snow_man@bk.ru>
2020-04-17 10:34:27 +0400

    циклы переписаны на замыкания

--- src/clousures.groovy
+++ src/clousures.groovy
@@ -1,19 +1,19 @@
 //создаем массив и заполняем значениями
-def arr = [2, 3, 8, 500, -5, 0 ,101]
+arr = [2, 3, 8, 500, -5, 0 ,101]
 //описываем замыкание, которое сортирует по возрастанию
 def temp = 0
-def mergeSort = { a ->
+def Sort = { a ->
 
-        a.eachWithIndex {it, index ->
-            for( j = a.size()-1; j > index; j-- ){
-            if (a[j - 1] > a[j]) {
-                temp = a[j-1]
-                a[j - 1] = a[j]
-                a[j] = temp
+    a.each {                        //для каждого элемента в массиве
+        (a.size()-1..1).each {      //range индексов с конца, для каждого из них
+            if (a[it - 1] > a[it]) {//собственно перестановка
+                temp = a[it-1]
+                a[it - 1] = a[it]
+                a[it] = temp
             }
             }
         }
     }
 
 //вызываем замыкание
-print mergeSort(arr)
+print Sort(arr)
