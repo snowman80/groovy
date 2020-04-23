46914b8d50fd06b936ba35514004be3e591e307b
Alexey Osipov <snow_man@bk.ru>
2020-04-23 18:27:50 +0400

    изменен алгоритм суммирования

--- files_scripts/src/StringTotal.groovy
+++ files_scripts/src/StringTotal.groovy
@@ -5,24 +5,18 @@ def path = "D:\\EPAM\\groovy_projects\\files_scripts\\"
 def separator = System.getProperty("line.separator")
 //файл источник
 def source = new File(path, "result.csv")
-def sb = new StringBuilder()
 //выходной файл
 def result = new File(path, "result_tmp.csv")
-String[] val = ""
-int summa = 0
+def sb = new StringBuilder()
+
 //проходим по файлу построчно, записывая значения, разделенные ',' во временный массив
-//после этого суммируем значения в переменную summa
-//записываем в sb первоначальную строку + сумму и сепаратор, обнуляем временный массив и сумму
-source.eachLine {val = it.split(',')
-    (0..val.size()-1).each {
-        summa = summa + Integer.parseInt(val[it])
-    }
-        sb << it + "," + summa + separator
-        summa = 0
-        val = ""
+//записываем в sb первоначальную строку + сумму и сепаратор
+source.eachLine {
+    def val = it.split(',')
+        sb << it + val.sum() {Integer.parseInt(it)} + separator
     }
 //записываем в выходной  файл итоговую строку
-result << sb
+result << sb.toString()
 //удаляем исходный файл
 source.delete()
 //переименовываем выходной файл в имя исходного
