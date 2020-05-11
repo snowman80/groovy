e7338308aac54dd9f23789417e0baba644efbd09
Alexey Osipov <snow_man@bk.ru>
2020-05-11 19:29:33 +0400

    добавил разделение суммы и  исходной строки, переименовал массив в более информативный

--- files_scripts/src/StringTotal.groovy
+++ files_scripts/src/StringTotal.groovy
@@ -10,10 +10,10 @@ def result = new File(path, "result_tmp.csv")
 def sb = new StringBuilder()
 
 //проходим по файлу построчно, записывая значения, разделенные ',' во временный массив
-//записываем в sb первоначальную строку + сумму и сепаратор
+//записываем в sb первоначальную строку + разделительную , + сумму и сепаратор
 source.eachLine {
-    def val = it.split(',')
-        sb << it + val.sum() {Integer.parseInt(it)} + separator
+    def values = it.split(',')
+        sb << it + "," + values.sum() {Integer.parseInt(it)} + separator
     }
 //записываем в выходной  файл итоговую строку
 result << sb.toString()
