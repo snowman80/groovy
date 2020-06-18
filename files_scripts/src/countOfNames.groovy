1505f130825424836e33f4899edf97573120f2b0
Alexey Osipov <snow_man@bk.ru>
2020-06-18 12:33:33 +0400

    condition of null name of input file deleted stringbuilders

--- files_scripts/src/countOfNames.groovy
+++ files_scripts/src/countOfNames.groovy
@@ -1,31 +1,27 @@
 import groovy.xml.XmlSlurper
 
 def path = "D:\\EPAM\\groovy_projects\\files_scripts\\"
-File source = new File(path, "usa_employers.xml")
+File source = new File(path, "usa_employees.xml")
 def separator = System.getProperty("line.separator")
 def list = new XmlSlurper().parse(source)
-//решаем одновременно 2 и 3 задачи, StringBuilder для каждого типа
-def sbFN = new StringBuilder()
-def sbLN = new StringBuilder()
-//обращаемся к ноде по ее пути и записываем в sb
-list.employee.personal_info.first_name.each {sbFN << it.toString() + separator}
-list.employee.personal_info.last_name.each {sbLN << it.toString() + separator}
+//решаем одновременно 2 и 3 задачи
 //для подчета и записи кол-ва используем Мар
 Map<String, Integer> mapFN = new HashMap<String, Integer>()
-sbFN.eachLine  {
+//обращаемся к ноде по ее пути и записываем в Map, считая кол-во
+list.employee.personal_info.first_name.each {
     Integer count = mapFN.get(it)
-    if ( count == null ) {
+    if (!count) {
         count = 0
     }
-    mapFN.put(it, count + 1)
+    mapFN.put(it.toString(), count + 1)
 }
 Map<String, Integer> mapLN = new HashMap<String, Integer>()
-sbLN.eachLine  {
+list.employee.personal_info.last_name.each {
     Integer count = mapFN.get(it)
-    if ( count == null ) {
+    if (!count) {
         count = 0
     }
-    mapLN.put(it, count + 1)
+    mapLN.put(it.toString(), count + 1)
 }
 //читаем Мар в sb
 def resultFN = new StringBuilder()
